499c63a62074829fd4349897799a6b2b507bc1a9
timothy okooboh <okoobohtimothy@gmail.com>
2023-11-04 17:35:01 +0100

    se tup eslint on the ui library package

---
+++ packages/ui-library/.eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  // https://eslint.org/docs/rules/no-undef#nodejs
+  env: {
+    node: true,
+  },
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:vue/vue3-recommended",
+    "eslint-config-prettier",
+  ],
+};
