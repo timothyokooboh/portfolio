408ccf3c7bfc67bce67d18b0f3d41e38bc5fe485
timothy okooboh <okoobohtimothy@gmail.com>
2023-11-11 19:42:23 +0100

    continue slicing of landing page

--- apps/portfolio/app.vue
+++ apps/portfolio/app.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="p-8 md:py-16 md:px-[39px] lg:px-[165px]">
-    <TheHeader />
+    <TheHeader
+      class="fixed z-10 bg-white w-full top-0 left-0 p-8 md:py-16 md:px-[39px] lg:px-[165px]"
+    />
 
-    <NuxtPage class="mt-[40px]" />
+    <NuxtPage class="mt-[120px]" />
   </div>
 </template>
