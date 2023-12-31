a9433b14fe9644231c3da025a09f3a368efba960
timothy okooboh <okoobohtimothy@gmail.com>
2023-12-31 16:42:54 +0100

    add nitro configuration

--- apps/portfolio/nuxt.config.ts
+++ apps/portfolio/nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  nitro: {
+    preset: "vercel",
+    output: {
+      dir: ".vercel/output",
+    },
+  },
   modules: [
     "@nuxtjs/tailwindcss",
     "nuxt-icon",
