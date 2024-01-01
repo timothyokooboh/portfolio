93055ed957920b2f74a81d47a59ad27e9857aa05
timothy okooboh <okoobohtimothy@gmail.com>
2024-01-01 13:04:02 +0100

    add max width

--- apps/portfolio/app.vue
+++ apps/portfolio/app.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="flex flex-col min-h-screen">
-    <div class="p-8 md:py-16 md:px-[39px] lg:px-[165px] mb-auto">
+    <div
+      class="max-w-[2000px] p-8 mx-auto md:py-16 md:px-[39px] lg:px-[165px] mb-auto"
+    >
       <TheHeader
         class="fixed z-10 bg-white w-full top-0 left-0 p-8 md:py-16 md:px-[39px] lg:px-[165px]"
       />
